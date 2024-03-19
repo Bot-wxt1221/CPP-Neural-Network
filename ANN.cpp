b1b363205ff81b294c59de459b2540de699e3cea
Zachary Gao <gyc.zachary@gmail.com>
2024-03-19 15:09:14 +0800

    。

--- ANN.cpp
+++ ANN.cpp
@@ -1,49 +1,49 @@
-// #pragma GCC target("avx")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("inline")
-#pragma GCC optimize("-fgcse")
-#pragma GCC optimize("-fgcse-lm")
-#pragma GCC optimize("-fipa-sra")
-#pragma GCC optimize("-ftree-pre")
-#pragma GCC optimize("-ftree-vrp")
-#pragma GCC optimize("-fpeephole2")
-#pragma GCC optimize("-ffast-math")
-#pragma GCC optimize("-fsched-spec")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("-falign-jumps")
-#pragma GCC optimize("-falign-loops")
-#pragma GCC optimize("-falign-labels")
-#pragma GCC optimize("-fdevirtualize")
-#pragma GCC optimize("-fcaller-saves")
-#pragma GCC optimize("-fcrossjumping")
-#pragma GCC optimize("-fthread-jumps")
-#pragma GCC optimize("-funroll-loops")
-#pragma GCC optimize("-fwhole-program")
-#pragma GCC optimize("-freorder-blocks")
-#pragma GCC optimize("-fschedule-insns")
-#pragma GCC optimize("inline-functions")
-#pragma GCC optimize("-ftree-tail-merge")
-#pragma GCC optimize("-fschedule-insns2")
-#pragma GCC optimize("-fstrict-aliasing")
-#pragma GCC optimize("-fstrict-overflow")
-#pragma GCC optimize("-falign-functions")
-#pragma GCC optimize("-fcse-skip-blocks")
-#pragma GCC optimize("-fcse-follow-jumps")
-#pragma GCC optimize("-fsched-interblock")
-#pragma GCC optimize("-fpartial-inlining")
-#pragma GCC optimize("no-stack-protector")
-#pragma GCC optimize("-freorder-functions")
-#pragma GCC optimize("-findirect-inlining")
-#pragma GCC optimize("-fhoist-adjacent-loads")
-#pragma GCC optimize("-frerun-cse-after-loop")
-#pragma GCC optimize("inline-small-functions")
-#pragma GCC optimize("-finline-small-functions")
-#pragma GCC optimize("-ftree-switch-conversion")
-#pragma GCC optimize("-foptimize-sibling-calls")
-#pragma GCC optimize("-fexpensive-optimizations")
-#pragma GCC optimize("-funsafe-loop-optimizations")
-#pragma GCC optimize("inline-functions-called-once")
-#pragma GCC optimize("-fdelete-null-pointer-checks")
+// // #pragma GCC target("avx")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("inline")
+// #pragma GCC optimize("-fgcse")
+// #pragma GCC optimize("-fgcse-lm")
+// #pragma GCC optimize("-fipa-sra")
+// #pragma GCC optimize("-ftree-pre")
+// #pragma GCC optimize("-ftree-vrp")
+// #pragma GCC optimize("-fpeephole2")
+// #pragma GCC optimize("-ffast-math")
+// #pragma GCC optimize("-fsched-spec")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC optimize("-falign-jumps")
+// #pragma GCC optimize("-falign-loops")
+// #pragma GCC optimize("-falign-labels")
+// #pragma GCC optimize("-fdevirtualize")
+// #pragma GCC optimize("-fcaller-saves")
+// #pragma GCC optimize("-fcrossjumping")
+// #pragma GCC optimize("-fthread-jumps")
+// #pragma GCC optimize("-funroll-loops")
+// #pragma GCC optimize("-fwhole-program")
+// #pragma GCC optimize("-freorder-blocks")
+// #pragma GCC optimize("-fschedule-insns")
+// #pragma GCC optimize("inline-functions")
+// #pragma GCC optimize("-ftree-tail-merge")
+// #pragma GCC optimize("-fschedule-insns2")
+// #pragma GCC optimize("-fstrict-aliasing")
+// #pragma GCC optimize("-fstrict-overflow")
+// #pragma GCC optimize("-falign-functions")
+// #pragma GCC optimize("-fcse-skip-blocks")
+// #pragma GCC optimize("-fcse-follow-jumps")
+// #pragma GCC optimize("-fsched-interblock")
+// #pragma GCC optimize("-fpartial-inlining")
+// #pragma GCC optimize("no-stack-protector")
+// #pragma GCC optimize("-freorder-functions")
+// #pragma GCC optimize("-findirect-inlining")
+// #pragma GCC optimize("-fhoist-adjacent-loads")
+// #pragma GCC optimize("-frerun-cse-after-loop")
+// #pragma GCC optimize("inline-small-functions")
+// #pragma GCC optimize("-finline-small-functions")
+// #pragma GCC optimize("-ftree-switch-conversion")
+// #pragma GCC optimize("-foptimize-sibling-calls")
+// #pragma GCC optimize("-fexpensive-optimizations")
+// #pragma GCC optimize("-funsafe-loop-optimizations")
+// #pragma GCC optimize("inline-functions-called-once")
+// #pragma GCC optimize("-fdelete-null-pointer-checks")
 #include<bits/stdc++.h>
 // #define ONLINE_JUDGE
 #define INPUT_DATA_TYPE int
@@ -57,7 +57,7 @@ OUTPUT_SIZE: 输出向量长度
 NET_DEEP:    中间层深度
 NET_SIZE:    中间层向量长度
 */
-const int INPUT_SIZE=784,OUTPUT_SIZE=10,NET_DEEP=10,NET_SIZE=800,MAX_PIC=1000010;
+const int PIC_SIZE=28,INPUT_SIZE=784,OUTPUT_SIZE=10,NET_DEEP=8,NET_SIZE=900,MAX_PIC=1000010;
 std::mt19937 mt(time(0));
 
 inline double sigmoid(double x,char flg) {
@@ -270,10 +270,10 @@ inline void addGroupDelta(){
 
 inline void init(){
     register int i;
-    register double WrdK=0.5,BrdK=0;
+    register double WrdK=1.0,BrdK=0;
     register double BrdA=0,BrdB=2.0/INPUT_SIZE;register int flgB=0;
-    register double WrdA_i=0,WrdB_i=2.0/INPUT_SIZE;register int flgW_i=2;
-    register double WrdA_o=0,WrdB_o=2.0/OUTPUT_SIZE;register int flgW_o=2;
+    register double WrdA_i=0,WrdB_i=0.03;register int flgW_i=2;
+    register double WrdA_o=0,WrdB_o=0.06;register int flgW_o=2;
     
     
     W[0].init(INPUT_SIZE,NET_SIZE,WrdK,flgW_i,WrdA_i,WrdB_i);
@@ -329,18 +329,6 @@ inline void getNet(){
 first:input matrix
 second:ans matrix
 */
-// inline std::pair<MATRIX,MATRIX> getData(){
-//     register int i,a=mt()&1;
-//     // a=1;
-//     MATRIX input,ans;
-//     input.init(1,INPUT_SIZE,0);
-//     ans.init(1,OUTPUT_SIZE,0);
-    
-//     for(i=0;i<INPUT_SIZE;++i) input.mat[0][i]=i^a;
-//     for(i=0;i<OUTPUT_SIZE;++i) ans.mat[0][i]=input.mat[0][(int)std::min(INPUT_SIZE-1.0,1.0*i*INPUT_SIZE/OUTPUT_SIZE)];
-    
-//     return {input,ans};
-// }
 int res[100010],now=0,cntPic;
 void getPic(int n){
     cntPic=n,now=0;
@@ -353,7 +341,13 @@ void getPic(int n){
         for(j=0;j<INPUT_SIZE;++j) inputPic[i].mat[0][j]=read()*1.0/255;
     }
 }
-inline std::pair<MATRIX,MATRIX> getData(char rd){
+std::pair<int,int> getPos(int x){
+    return {x/PIC_SIZE,x%PIC_SIZE};
+}
+int getId(int x,int y){
+    return x*PIC_SIZE+y;
+}
+inline std::pair<MATRIX,MATRIX> getData(char rd,char addNoise){
     if(rd){
         std::uniform_int_distribution<int> rd(0,cntPic-1);
         now=rd(mt);
@@ -362,9 +356,24 @@ inline std::pair<MATRIX,MATRIX> getData(char rd){
     MATRIX input,ans;
     input.init(1,INPUT_SIZE,0,0);
     ans.init(1,OUTPUT_SIZE,0,0);
-    
+
     input=inputPic[now];
     ans.mat[0][res[now]]=1;
+
+    if(addNoise){
+        register int i,dx,dy,x,y;
+        MATRIX tmp=inputPic[now];
+        std::uniform_int_distribution<int> rdBXY(-1,1);
+        dx=rdBXY(mt),dy=rdBXY(mt);
+        for(i=0;i<INPUT_SIZE;++i){
+            auto pos=getPos(i);
+            x=pos.first+dx,y=pos.second+dy;
+            x=std::max(0,std::min(x,PIC_SIZE-1));
+            y=std::max(0,std::min(y,PIC_SIZE-1));
+            input.mat[0][i]=tmp.mat[0][getId(x,y)];
+        }
+    }
+
     return {input,ans};
 }
 int getOutputAns(MATRIX output){
@@ -384,7 +393,7 @@ void train(int cnt,int grp,int outGap,double learn,char randomInput,int round=-1
         if(i%1000==0) cntRight=cntAll=0;
 
         for(j=0;j<grp;++j){
-            auto pr=getData(randomInput);
+            auto pr=getData(randomInput,0);
             input=pr.first,ans=pr.second;
             output=work(input);
 
@@ -438,7 +447,7 @@ void test(int n,int all,int outGap,char rd){
     MATRIX input,output,ans;
     getTestPic(all);
     for(i=0;i<n;++i){
-        auto pr=getData(rd);
+        auto pr=getData(rd,0);
         input=pr.first,ans=pr.second;
         output=work(input);
 
@@ -468,8 +477,8 @@ int main(){
     getPic(60000);
     getNet();
 
-    train(10000,10,10,0.1,1);
-    test(10000,10000,50,0);
+    train(10000,1,10,0.0002,1);
+    test(10000,10000,500,0);
 
     save();
 
